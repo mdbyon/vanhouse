c7ad23021f43949c18392461892511e3558badf1
mbruz <mattbruzik@gmail.com>
2018-11-04 10:30:20 -0500

    Routing
    added basic routing

--- vanpublic/src/App.js
+++ vanpublic/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+
+const Index = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+const Users = () => <h2>Users</h2>;
 
 class App extends Component {
   render() {
@@ -8,12 +13,28 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Welcome to Vanhouse
-          </p>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <h1>Vanhouse</h1>
+          <Router>
+            <div>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/about/">About</Link>
+                  </li>
+                  <li>
+                    <Link to="/users/">Users</Link>
+                  </li>
+                </ul>
+              </nav>
+
+              <Route path="/" exact component={Index} />
+              <Route path="/about/" component={About} />
+              <Route path="/users/" component={Users} />
+            </div>
+          </Router>          
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -22,10 +43,9 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> 
       </div>
     );
   }
 }
-
 export default App;
