a9e64cb846b356fff87ec294cc23568efbc4ff7c
Michael Byon <m.byon115@gmail.com>
2018-11-24 22:12:14 -0500

    Added in individual pages for jeff. Click on profile bubble to see

--- vanpublic/src/App.js
+++ vanpublic/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 import Home from './Home'
+import {JeffWarner} from "./JeffWarner";
 
 const About = () => <h2>About</h2>;
 const Users = () => <h2>Users</h2>;
@@ -10,11 +11,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <Router>
+          <Router onUpdate={() => window.scrollTo(0, 0)}>
             <div>
               <Route path="/" exact component={Home} />
               <Route path="/about/" component={About} />
               <Route path="/users/" component={Users} />
+              <Route path="/jeff_warner/" component={JeffWarner} />
             </div>
           </Router>
       </div>
---
+++ vanpublic/src/constants/profileConstants.js
@@ -0,0 +1,11 @@
+export const JeffWarnerBio = {
+  name: 'Jeff Warner',
+  employer: 'Raytheon',
+  school: 'UConn',
+}
+
+export const MattBruzikBio = {
+  name: 'Matt Bruzik',
+  employer: 'Tallan',
+  school: 'UConn',
+}
